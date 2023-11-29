af3363f2cb699c3f2252f7a3e0ca3aacffef4da4
Sonya Sergeeva <sonya451f@gmail.com>
2023-11-29 23:40:32 +0700

    Add model Customer

--- coursework/work/admin.py
+++ coursework/work/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Customer
+
 
 # Register your models here.
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        'customer_id', 'customer_name', 'customer_lastname', 'customer_email',
+        'customer_phone', 'customer_datebirth', 'customer_login')
+
+
+admin.site.register(Customer, CustomerAdmin)
--- coursework/work/models.py
+++ coursework/work/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    """Customer model"""
+
+    class Meta:
+        db_table = 'customers'
+        managed = False
+        verbose_name = "Покупатель"
+        verbose_name_plural = "Покупатели"
+
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(verbose_name="Имя", max_length=50,
+                                     null=True, blank=True)
+    customer_lastname = models.CharField(verbose_name="Фамилия", max_length=50,
+                                         null=True, blank=True)
+    customer_surename = models.CharField(verbose_name="Отчество",
+                                         max_length=50, null=True, blank=True)
+    customer_email = models.EmailField(verbose_name="Почта", unique=True,
+                                       null=True, blank=True)
+    customer_phone = models.CharField(verbose_name="Телефон", max_length=15,
+                                      null=True, blank=True)
+    customer_datebirth = models.DateField(verbose_name="Дата рождения",
+                                          null=True, blank=True)
+    customer_login = models.CharField(verbose_name="Логин", max_length=100,
+                                      unique=True)
+    customer_password = models.CharField(verbose_name="Пароль", max_length=100,
+                                         unique=True)
+    customer_photo = models.TextField(verbose_name="Фотография", null=True,
+                                      blank=True)
