51ea25ce48c0b4d8cb4a706c7cca4044783b6550
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-01 22:50:37 +0700

    Add models Addresses, Status, Categories

--- coursework/work/admin.py
+++ coursework/work/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customers, Addresses, Status, Categories
 
 
 # Register your models here.
 
 
-class CustomerAdmin(admin.ModelAdmin):
+class CustomersAdmin(admin.ModelAdmin):
     list_display = (
         'customer_id', 'customer_name', 'customer_lastname', 'customer_email',
         'customer_phone', 'customer_datebirth', 'customer_login')
 
 
-admin.site.register(Customer, CustomerAdmin)
+class AddressesAdmin(admin.ModelAdmin):
+    list_display = (
+        'address_id', 'ad_customer', 'ad_country', 'ad_region',
+        'ad_city', 'ad_street', 'ad_house', 'ad_building', 'ad_apartment',
+        'ad_index')
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('status_id', 'status_name')
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'category_name')
+
+
+admin.site.register(Customers, CustomersAdmin)
+admin.site.register(Addresses, AddressesAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Categories, CategoriesAdmin)
--- coursework/work/models.py
+++ coursework/work/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-class Customer(models.Model):
-    """Customer model"""
+class Customers(models.Model):
+    """Customers model"""
 
     class Meta:
         db_table = 'customers'
@@ -29,3 +29,53 @@ class Customer(models.Model):
                                          unique=True)
     customer_photo = models.TextField(verbose_name="Фотография", null=True,
                                       blank=True)
+
+
+class Addresses(models.Model):
+    """Addresses model"""
+
+    class Meta:
+        db_table = 'addresses'
+        managed = False
+        verbose_name = "Адрес"
+        verbose_name_plural = "Адреса"
+
+    address_id = models.AutoField(primary_key=True)
+    ad_customer = models.ForeignKey('Customers', on_delete=models.RESTRICT,
+                                    db_column='ad_customer')
+    ad_country = models.CharField(verbose_name="Страна", max_length=100)
+    ad_region = models.CharField(verbose_name="Регион/край", max_length=100)
+    ad_city = models.CharField(verbose_name="Город", max_length=100)
+    ad_street = models.CharField(verbose_name="Улица", max_length=100)
+    ad_house = models.IntegerField(verbose_name="Дом")
+    ad_building = models.IntegerField(verbose_name="Строение", null=True,
+                                      blank=True)
+    ad_apartment = models.IntegerField(verbose_name="Квартира", null=True,
+                                       blank=True)
+    ad_index = models.IntegerField(verbose_name="Индекс")
+
+
+class Status(models.Model):
+    """Status model"""
+
+    class Meta:
+        db_table = "status"
+        managed = False
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
+    status_id = models.AutoField(primary_key=True)
+    status_name = models.CharField(verbose_name="Статус заказа", max_length=20)
+
+
+class Categories(models.Model):
+    """Categories model"""
+
+    class Meta:
+        db_table = 'categories'
+        managed = False
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=70)
