62607e6d108765b98d1c680b4be25ffb3876b155
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-02 15:15:05 +0700

    Add models Products, Supplies, Orders, OrserItems

--- coursework/work/admin.py
+++ coursework/work/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Customers, Addresses, Status, Categories
+from .models import Customers, Addresses, Status, Categories, Products, \
+    Supplies, Orders, OrderItems
 
 
 # Register your models here.
@@ -26,7 +27,29 @@ class CategoriesAdmin(admin.ModelAdmin):
     list_display = ('category_id', 'category_name')
 
 
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id', 'prod_name', '__description__', 'prod_photo',
+        'prod_amount', 'prod_category', 'prod_sell_price', 'prod_supply_price')
+
+
+class SuppliesAdmin(admin.ModelAdmin):
+    list_display = ('supply_id', 'sup_product', 'sup_amount')
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'order_date', 'ord_customer', 'ord_status')
+
+
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ('oi_order', 'oi_product', 'oi_amount')
+
+
 admin.site.register(Customers, CustomersAdmin)
 admin.site.register(Addresses, AddressesAdmin)
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Supplies, SuppliesAdmin)
+admin.site.register(Orders, OrdersAdmin)
+admin.site.register(OrderItems, OrderItemsAdmin)
--- coursework/work/models.py
+++ coursework/work/models.py
@@ -65,7 +65,8 @@ class Status(models.Model):
         verbose_name_plural = "Статусы"
 
     status_id = models.AutoField(primary_key=True)
-    status_name = models.CharField(verbose_name="Статус заказа", max_length=20)
+    status_name = models.CharField(verbose_name="Статус заказа", max_length=20,
+                                   unique=True)
 
 
 class Categories(models.Model):
@@ -78,4 +79,88 @@ class Categories(models.Model):
         verbose_name_plural = "Категории"
 
     category_id = models.AutoField(primary_key=True)
-    category_name = models.CharField(max_length=70)
+    category_name = models.CharField(verbose_name="Категория товара",
+                                     max_length=70, unique=True)
+
+
+class Products(models.Model):
+    """Products model"""
+
+    class Meta:
+        db_table = "products"
+        managed = False
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    product_id = models.AutoField(primary_key=True)
+    prod_name = models.CharField(verbose_name="Название товара",
+                                 max_length=100, unique=True)
+    prod_description = models.TextField(verbose_name="Описание товара",
+                                        null=True, blank=True)
+    prod_photo = models.TextField(verbose_name="Ссылка на фото товара",
+                                  null=True, blank=True)
+    prod_amount = models.IntegerField(verbose_name="Количество товара",
+                                      null=True, blank=True, default=0)
+    prod_category = models.ForeignKey('Categories', on_delete=models.RESTRICT,
+                                      db_column='prod_category')
+    prod_sell_price = models.DecimalField(verbose_name="Цена продажи",
+                                          max_digits=10, decimal_places=2,
+                                          default=0.00)
+    prod_supply_price = models.DecimalField(verbose_name="Цена поставки",
+                                            max_digits=10, decimal_places=2,
+                                            default=0.00)
+
+    def __description__(self):
+        return self.prod_description[:40] + "..." \
+            if self.prod_description else 'Описание отсутсвует'
+
+
+class Supplies(models.Model):
+    """Supplies model"""
+
+    class Meta:
+        db_table = 'supplies'
+        managed = False
+        verbose_name = "Поставка"
+        verbose_name_plural = "Поставки"
+
+    supply_id = models.AutoField(primary_key=True)
+    sup_product = models.ForeignKey('Products', on_delete=models.RESTRICT,
+                                    db_column='sup_product')
+    sup_amount = models.IntegerField(verbose_name='Количество товара')
+
+
+class Orders(models.Model):
+    """Orders model"""
+
+    class Meta:
+        db_table = 'orders'
+        managed = False
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+    order_id = models.AutoField(primary_key=True)
+    order_date = models.DateTimeField(verbose_name='Дата заказа',
+                                      auto_now_add=True)
+    ord_customer = models.ForeignKey('Customers', on_delete=models.RESTRICT,
+                                     db_column='ord_customer')
+    ord_status = models.ForeignKey('Status', on_delete=models.RESTRICT,
+                                   db_column='ord_status')
+
+
+class OrderItems(models.Model):
+    """OrderItems model"""
+
+    class Meta:
+        db_table = 'order_items'
+        managed = False
+        verbose_name = "Единица заказа"
+        verbose_name_plural = "Единицы заказов"
+        unique_together = ('oi_order', 'oi_product')
+
+    oi_id = models.AutoField(primary_key=True)
+    oi_order = models.ForeignKey('Orders', on_delete=models.RESTRICT,
+                                 db_column='oi_order')
+    oi_product = models.ForeignKey('Products', on_delete=models.RESTRICT,
+                                   db_column='oi_product')
+    oi_amount = models.IntegerField(default=1)
