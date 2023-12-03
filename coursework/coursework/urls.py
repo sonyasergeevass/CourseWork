4af07cfb21c70918c6f72b78e6a207786ae6216a
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-03 21:19:30 +0700

    Add view and url to render products_list and site header

--- coursework/coursework/urls.py
+++ coursework/coursework/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from work import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
+    path("", views.product_list, name="welcome"),
 ]
--- coursework/work/views.py
+++ coursework/work/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-import math
-# Create your views here.
+from .models import Products
+
+
+# def welcome(request):
+#     return render(request, 'main.html')
+
+
+def product_list(request):
+    products = Products.objects.all()
+    return render(request, 'show_products.html',
+                  {'products': products})
