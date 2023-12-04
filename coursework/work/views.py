f6e08d8f8b40af74f2d7ffe84b3020b261018342
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-04 23:37:22 +0700

    Add link converter and func for search query return

--- coursework/work/views.py
+++ coursework/work/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products, Categories
 
 
 # def welcome(request):
 #     return render(request, 'main.html')
+def convert_to_direct_link(gdrive_url):
+    file_id = gdrive_url.split('/d/')[1].split('/view')[0]
+    return f'https://drive.google.com/uc?id={file_id}'
+
+
+def categories_list():
+    return Categories.objects.all()
 
 
 def product_list(request):
-    products = Products.objects.all()
+    search_query = request.GET.get('q', '')
+    if search_query:
+        products = Products.objects.filter(name__icontains=search_query)
+    else:
+        products = Products.objects.all()
+    for product in products:
+        if product.prod_photo:
+            product.prod_photo = convert_to_direct_link(product.prod_photo)
+    categories = categories_list()
     return render(request, 'show_products.html',
-                  {'products': products})
+                  {'products': products, 'categories': categories})
+
+
+def search_products(request):
+    if request.method == 'GET':
+        query = request.GET.get('query', '')
+        results = Products.objects.filter(prod_name__icontains=query)
+        return render(request, 'search_results.html',
+                      {'results': results})
