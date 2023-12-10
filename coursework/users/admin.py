6bf002b2a135db62da28fef99975e00f98df3edd
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-10 18:11:18 +0700

    Customize the display of the UserAdmin in admin

--- coursework/users/admin.py
+++ coursework/users/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    pass
+    list_display = (
+        "username", "email", "last_name", "first_name", "sure_name", "phone",
+        "datebirth",
+        "is_staff")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": (
+            "first_name", "last_name", "sure_name", "email", "phone",
+            "datebirth")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Additional information"), {"fields": ("photo",)}),
+    )
