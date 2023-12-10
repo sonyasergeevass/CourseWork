780bacf61d1959467e1e792f51fab231d4ea8f9c
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-10 21:05:09 +0700

    Add url to show profile info

--- coursework/users/urls.py
+++ coursework/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
 
     path('register/', views.Register.as_view(), name='register'),
+    path('user_profile/<int:user_id>/', views.user_profile, name="user")
 ]
