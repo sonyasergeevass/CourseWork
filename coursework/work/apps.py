7b65083cb0a9848b3ad10991f4c633b5fe28fe7e
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-10 18:01:58 +0700

    Add verbose names

--- coursework/work/apps.py
+++ coursework/work/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class WorkConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "work"
+    verbose_name = "База данных интернет магазина"
