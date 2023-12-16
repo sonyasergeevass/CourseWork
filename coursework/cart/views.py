b964d6a566e34230d9657dbb94b3c1cd632e07aa
Sonya Sergeeva <sonya451f@gmail.com>
2023-12-16 18:28:51 +0700

    Add code to save items from cart to db

--- coursework/cart/views.py
+++ coursework/cart/views.py
@@ -1,6 +1,7 @@
+from django.db import transaction
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from work.models import Products
+from work.models import Products, Orders, OrderItems, Status
 from .cart import Cart
 from .forms import CartAddProductForm
 
@@ -15,12 +16,33 @@ def cart_add(request, product_id):
         cart.add(product=product,
                  quantity=cd['quantity'],
                  update_quantity=cd['update'])
+
+        with (transaction.atomic()):
+            order = Orders.objects.filter(
+                ord_customer=request.user,
+                ord_status__status_name='Временный').first()
+            if not order:
+                order = Orders.objects.create(ord_customer=request.user,
+                                              ord_status=Status.objects.get(
+                                                  status_name='Временный'))
+        for item in cart:
+            product = Products.objects.get(
+                product_id=item['product']['product_id'])
+            order_item, created = OrderItems.objects.get_or_create(
+                oi_order=order,
+                oi_product=product,
+                oi_amount=item['quantity']
+            )
+            if not created:
+                order_item.oi_amount += item['quantity']
+                order_item.save()
+
         # request.session['added_product'] = {'prod_photo': converted_photo}
         # print(f"{type(request.session['added_product'].get('prod_photo'))}")
-        print(
-            f"Product {product.prod_name} added to the cart with quantity "
-            f"{cd['quantity']}.")
-        print(f"Cart contents: {cart.cart}")
+        # print(
+        #     f"Product {product.prod_name} added to the cart with quantity "
+        #     f"{cd['quantity']}.")
+        # print(f"Cart contents: {cart.cart}")
     # else:
     #     print("Form is not valid.")
     #     print(form.errors)
